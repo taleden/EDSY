72381fee69ab544caa82af9804f44709ba95b647
taleden <taleden0@gmail.com>
2020-04-07 13:35:31 -0500

    updated 1D Meta Alloy Hull Reinforcement mass

--- eddb.js
+++ eddb.js
@@ -10,9 +10,9 @@ Frontier Customer Services (https://forums.frontier.co.uk/threads/elite-dangerou
 */
 'use strict';
 var eddb = {
-	version : 36241,
-	edsy_versions_db : [36241,36241,36241,36241], /* HTML,CSS,DB,JS */
-	edsy_lastmodified_db : 20200403,
+	version : 37021,
+	edsy_versions_db : [36241,36241,37021,36241], /* HTML,CSS,DB,JS */
+	edsy_lastmodified_db : 20200407,
 	ship : {
 		 1 : {
 			fdid:128049249, fdname:'SideWinder', eddbid:18,
@@ -3099,7 +3099,7 @@ var eddb = {
 		
 		
 		 9151 : { mtype:'imahrp', cost:  7500, name:'Meta Alloy Hull Reinforcement Package', class:1, rating:'E', mass:  2, hullrnf: 72, caures:3.0, fdid:128793117, fdname:'Int_MetaAlloyHullReinforcement_Size1_Class1', eddbid:1664 }, // techbroker
-		 9141 : { mtype:'imahrp', cost: 22500, name:'Meta Alloy Hull Reinforcement Package', class:1, rating:'D', mass:  2, hullrnf: 99, caures:3.0, fdid:128793118, fdname:'Int_MetaAlloyHullReinforcement_Size1_Class2', eddbid:1665 }, // techbroker
+		 9141 : { mtype:'imahrp', cost: 22500, name:'Meta Alloy Hull Reinforcement Package', class:1, rating:'D', mass:  1, hullrnf: 99, caures:3.0, fdid:128793118, fdname:'Int_MetaAlloyHullReinforcement_Size1_Class2', eddbid:1665 }, // techbroker
 		 9251 : { mtype:'imahrp', cost: 18000, name:'Meta Alloy Hull Reinforcement Package', class:2, rating:'E', mass:  4, hullrnf:135, caures:3.0, fdid:128793119, fdname:'Int_MetaAlloyHullReinforcement_Size2_Class1', eddbid:1666 }, // techbroker
 		 9241 : { mtype:'imahrp', cost: 54000, name:'Meta Alloy Hull Reinforcement Package', class:2, rating:'D', mass:  2, hullrnf:171, caures:3.0, fdid:128793120, fdname:'Int_MetaAlloyHullReinforcement_Size2_Class2', eddbid:1667 }, // techbroker
 		 9351 : { mtype:'imahrp', cost: 42000, name:'Meta Alloy Hull Reinforcement Package', class:3, rating:'E', mass:  8, hullrnf:207, caures:3.0, fdid:128793121, fdname:'Int_MetaAlloyHullReinforcement_Size3_Class1', eddbid:1668 }, // techbroker
