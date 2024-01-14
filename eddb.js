b171a4d9b08470198a48c303e5ae1d0c72214d9c
taleden <taleden0@gmail.com>
2024-01-13 20:26:57 -0600

    added fdid for the 5E CRCR

--- eddb.js
+++ eddb.js
@@ -11,8 +11,8 @@ Frontier Customer Services (https://forums.frontier.co.uk/threads/elite-dangerou
 'use strict';
 var eddb = {
 	version : 308179901,
-	edsy_versions_db : [308159902,308159902,308179901,308179901], /* HTML,CSS,DB,JS */
-	edsy_lastmodified_db : 20240111,
+	edsy_versions_db : [308159902,308159902,308179903,308179901], /* HTML,CSS,DB,JS */
+	edsy_lastmodified_db : 20240113,
 	ship : {
 		 1 : {
 			fdid:128049249, fdname:'SideWinder', eddbid:18,
@@ -2975,7 +2975,7 @@ var eddb = {
 		  161 : { mtype:'icr', cost:  12560, name:'Anti-Corrosion Cargo Rack (Cap: 2)',  class:1, rating:'F', cargocap: 2, fdid:128681992, fdname:'Int_CorrosionProofCargoRack_Size1_Class2', eddbid:1552 }, // at Palin, Sedesi
 		  251 : { mtype:'icr', cost:    NaN, name:'Anti-Corrosion Cargo Rack (Cap: 4)',  class:2, rating:'E', cargocap: 4, fdid:     null, fdname:'Int_CorrosionProofCargoRack_Size2_Class1', eddbid:null, hidden:1 }, // never released
 		  451 : { mtype:'icr', cost:  94330, name:'Anti-Corrosion Cargo Rack (Cap: 16)', class:4, rating:'E', cargocap:16, fdid:128833944, fdname:'Int_CorrosionProofCargoRack_Size4_Class1', eddbid:1699 }, // human tech broker
-		  551 : { mtype:'icr', cost:    NaN, name:'Anti-Corrosion Cargo Rack (Cap: 32)', class:5, rating:'E', cargocap:32, fdid:     null, fdname:'Int_CorrosionProofCargoRack_Size5_Class1', eddbid:null, hidden:1 }, // CG reward
+		  551 : { mtype:'icr', cost:    NaN, name:'Anti-Corrosion Cargo Rack (Cap: 32)', class:5, rating:'E', cargocap:32, fdid:128957069, fdname:'Int_CorrosionProofCargoRack_Size5_Class1', eddbid:null, hidden:1 }, // CG reward
 		  651 : { mtype:'icr', cost:    NaN, name:'Anti-Corrosion Cargo Rack (Cap: 64)', class:6, rating:'E', cargocap:64, fdid:     null, fdname:'Int_CorrosionProofCargoRack_Size6_Class1', eddbid:null, hidden:1 }, // CG reward
 		
 		
